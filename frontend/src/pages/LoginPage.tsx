08373b071b5f5cee5aaf5af2fc226e4e5b6b82be
Thirasha <thirashapraween@gmail.com>
2026-04-05 13:32:57 +0530

    Update LoginPage.tsx

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -98,7 +98,7 @@ useMailStore.getState().setAdminFlags(!!res.data.isAdmin, (res.data.adminRole as
             />
           </div>
           <div className="form-group">
-            <label className="form-label">Password</label>
+            <label className="form-label">Password (C)</label>
             <input
               type="password"
               className="form-input"
